3222e1d0ccd65f9419e37e0af100c3b7a2ec7db6
FranciscoVieir <franciscovieira6397@gmail.com>
2022-10-12 16:23:39 -0300

    sending a piano simulator

---
+++ app.js
@@ -0,0 +1,12 @@
+const pianoKeys = document.querySelectorAll('.key');
+
+function playSound(newUrl) {
+  console.log(newUrl)
+  new Audio(newUrl).play()
+}
+
+pianoKeys.forEach((pianoKey, index) => {
+  const number = index < 9 ? '0' + (index + 1) : (index + 1)
+  const newUrl = "24-piano-keys/key" + number + '.mp3'
+  pianoKey.addEventListener('click', () => playSound(newUrl))
+})
